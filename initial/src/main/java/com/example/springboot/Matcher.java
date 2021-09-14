408948df4589dbb2f884613df9992c6c18be8d14
Eleanor Burke <eburke@scottlogic.com>
2021-09-14 15:59:12 +0100

    Added unit testing

--- initial/src/main/java/com/example/springboot/Matcher.java
+++ initial/src/main/java/com/example/springboot/Matcher.java
@@ -8,6 +8,8 @@ public class Matcher {   //every Java program has a class name which must match 
     public static ArrayList<Order> sellList = new ArrayList<Order>();
     public static ArrayList<Trade> transHist = new ArrayList<Trade>();
 
+    public Matcher() {}  //constructor
+
     public static void createOrder(String account, float price, int quantity, String action) {
         Order newOrder=new Order(account, price, quantity, action);
         processOrder(newOrder);
@@ -15,11 +17,43 @@ public class Matcher {   //every Java program has a class name which must match 
 
     public static void addOrder(Order order) {
         if (order.getAction() == "buy") {
-            buyList.add(order);
-            //sort list in descending
-        } else {
-            sellList.add(order);
-            //sort list in ascending
+            //sort buy list in descending
+            for (int i=0; i<buyList.size();i++) {
+                if (buyList.get(i).getPrice() >= order.getPrice()) {
+                    int len = buyList.size() - 1;
+                    if (i == len) {
+                        buyList.add(order);
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    buyList.add(i, order);
+                    break;
+                }
+            }
+                if (buyList.size() == 0) {
+                    buyList.add(order);
+                }
+            } else {
+            //sort sell list in ascending
+            for (int i=0; i<sellList.size();i++) {
+                if (sellList.get(i).getPrice() <= order.getPrice()) {
+                    int len = sellList.size() - 1;
+                    if (i == len) {
+                        sellList.add(order);
+                        break;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    sellList.add(i, order);
+                    break;
+                }
+            }
+            if (sellList.size() == 0) {
+                sellList.add(order);
+            }
         }
     }
 
@@ -29,7 +63,7 @@ public class Matcher {   //every Java program has a class name which must match 
             //pair off - transaction history
             newTrade(order.getAccount(), matchedOrder.getAccount(), matchedOrder.getPrice(), order.getQuantity(), order.getAction());
             order.setQuantity(0);
-            matchedOrder.setQuantity(0);; //make order in sell list null value
+            matchedOrder.setQuantity(0);  //make order in sell list null value
         }
         else if (order.getQuantity() <= matchedOrder.getQuantity()) {
             newTrade(order.getAccount(), matchedOrder.getAccount(), matchedOrder.getPrice(), order.getQuantity(), order.getAction());
@@ -45,14 +79,17 @@ public class Matcher {   //every Java program has a class name which must match 
     }
 
     public static void newTrade(String account1, String account2, float price, int quantity, String action) {
-        Trade newTrade=new Trade(account1, account2, price, quantity, action);
-        transHist.add(newTrade);
+        Trade trade=new Trade(account1, account2, price, quantity, action);
+        transHist.add(trade);
     }
 
     public static void removeNullOrders(ArrayList<Order> list) {
-        for (Order order:list) {
-            if(order.getQuantity()==0){
-                list.remove(order);
+        for (Order nullOrder:list) {
+            if(nullOrder.getQuantity()==0){
+                list.remove(nullOrder);
+                if (list.size()==0) {
+                    break;
+                }
             }
         }
     }
@@ -60,7 +97,7 @@ public class Matcher {   //every Java program has a class name which must match 
     public static void processOrder(Order order) {
         if (order.getAction() == "buy") {
             for (Order sellOrder:sellList) {
-                if (order.getPrice() == sellOrder.getPrice()) {
+                if (order.getAccount() == sellOrder.getAccount()) {
                     continue;
                 } else {
                     if (order.getPrice() >= sellOrder.getPrice()) {
@@ -79,7 +116,7 @@ public class Matcher {   //every Java program has a class name which must match 
             }
         } else {  //if action is sell
             for (Order buyOrder:buyList) {
-                if (order.getPrice() == buyOrder.getPrice()) {
+                if (order.getAccount() == buyOrder.getAccount()) {
                     continue;
                 } else {
                     if (order.getPrice() <= buyOrder.getPrice()) {
@@ -100,8 +137,12 @@ public class Matcher {   //every Java program has a class name which must match 
     }
 
     //private order book
+    public static ArrayList<Order> privBuy = new ArrayList<Order>();
+    public static ArrayList<Order> privSell = new ArrayList<Order>();
+    public static ArrayList<Trade> privTransHist = new ArrayList<Trade>();
+
+
     public static void privateOrderBuy(String currentAccount) {
-        ArrayList<Order> privBuy = new ArrayList<Order>();
         for (Order buyOrder:buyList) {
             if(buyOrder.getAccount()==currentAccount) {
                 privBuy.add(buyOrder);
@@ -110,7 +151,6 @@ public class Matcher {   //every Java program has a class name which must match 
     }
 
     public static void privateOrderSell(String currentAccount) {
-        ArrayList<Order> privSell = new ArrayList<Order>();
         for (Order sellOrder:sellList) {
             if(sellOrder.getAccount()==currentAccount) {
                 privSell.add(sellOrder);
@@ -119,7 +159,6 @@ public class Matcher {   //every Java program has a class name which must match 
     }
 
     public static void privateOrderTrans(String currentAccount) {
-        ArrayList<Trade> privTransHist = new ArrayList<Trade>();
         for (Trade trans:transHist) {
             if(trans.getAccount1()==currentAccount || trans.getAccount2()==currentAccount) {
                 privTransHist.add(trans);
@@ -128,8 +167,10 @@ public class Matcher {   //every Java program has a class name which must match 
     }
 
     //aggregated order book
+    public static HashMap<Float, Integer> aggSell = new HashMap<Float, Integer>();
+    public static HashMap<Float, Integer> aggBuy = new HashMap<Float, Integer>();
+
     public static void aggregatedSell() {
-        HashMap<Float, Integer> aggSell = new HashMap<Float, Integer>();
         for (Order sellOrder:sellList) {
             float price = sellOrder.getPrice();
             if (aggSell.containsKey(price)) {
@@ -142,7 +183,6 @@ public class Matcher {   //every Java program has a class name which must match 
     }
 
     public static void aggregatedBuy() {
-        HashMap<Float, Integer> aggBuy = new HashMap<Float, Integer>();
         for (Order buyOrder:buyList) {
             float price = buyOrder.getPrice();
             if (aggBuy.containsKey(price)) {
@@ -156,7 +196,10 @@ public class Matcher {   //every Java program has a class name which must match 
 
 
     public static void main(String[] args) {  //every program must contain the main() method
-        createOrder("user1", 10, 12, "buy");
+        createOrder("user1", 16, 4, "buy");
+        createOrder("user2", 16, 6, "buy");
+        aggregatedBuy();
+        System.out.println(aggBuy);
     }
 
 }
--- initial/src/test/java/com/example/springboot/MatcherTest.java
+++ initial/src/test/java/com/example/springboot/MatcherTest.java
@@ -1,14 +1,224 @@
 package com.example.springboot;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.ArrayList;
+
 
 public class MatcherTest {
 
-    //create orders here
+//    Matcher matcher;
 
-    //@Test
-    //public void ...test() {
-    // write test
-    // }
+    @BeforeEach
+    public void setUp() {
+
+        //Matcher matcher = new Matcher();
+        Matcher.buyList.clear();
+        Matcher.sellList.clear();
+        Matcher.transHist.clear();
+        Matcher.privBuy.clear();
+        Matcher.privSell.clear();
+        Matcher.privTransHist.clear();
+        Matcher.aggBuy.clear();
+        Matcher.aggSell.clear();
+    }
+
+
+    @Test
+    @DisplayName("Adds to buy list")
+    public void addToBuy() {
+        Matcher.createOrder("user1", 10, 12, "buy");
+        Matcher.createOrder("user2", 8, 8, "buy");
+        assertEquals(2, Matcher.buyList.size());
+    }
+
+    @Test
+    @DisplayName("Adds to sell list")
+    public void addToSell() {
+        Matcher.createOrder("user1", 12, 6, "sell");
+        assertEquals(1, Matcher.sellList.size());
+    }
+
+    @Test
+    @DisplayName("Adds to trade history")
+    public void addToTrades() {
+        Matcher.createOrder("user1", 12, 6, "sell");
+        Matcher.createOrder("user2", 12, 6, "buy");
+        assertEquals(1, Matcher.transHist.size());
+    }
+
+    @Test
+    @DisplayName("Buy list is ordered")
+    public void orderedBuy() {
+        Matcher.createOrder("user1", 16, 6, "buy");
+        Matcher.createOrder("user2", 12, 6, "buy");
+        Matcher.createOrder("user3", 14, 6, "buy");
+        assertEquals("user3", Matcher.buyList.get(1).getAccount());
+    }
+
+    @Test
+    @DisplayName("Sell list is ordered")
+    public void orderedSell() {
+        Matcher.createOrder("user1", 16, 6, "sell");
+        Matcher.createOrder("user2", 12, 6, "sell");
+        Matcher.createOrder("user3", 14, 6, "sell");
+        assertEquals("user2", Matcher.sellList.get(0).getAccount());
+    }
+
+    @Test
+    @DisplayName("No trade when same account")
+    public void noTrade() {
+        Matcher.createOrder("user1", 12, 6, "sell");
+        Matcher.createOrder("user1", 12, 6, "buy");
+        assertEquals(0, Matcher.transHist.size());
+    }
+
+    @Test
+    @DisplayName("Buyer quantity reduces after match")
+    public void reduceBuy() {
+        Matcher.createOrder("user1", 10, 12, "buy");
+        Matcher.createOrder("user2", 10, 8, "sell");
+        assertEquals(4, Matcher.buyList.get(0).getQuantity());
+    }
+
+    @Test
+    @DisplayName("Seller quantity reduces after match")
+    public void reduceSell() {
+        Matcher.createOrder("user1", 10, 12, "buy");
+        Matcher.createOrder("user2", 10, 14, "sell");
+        assertEquals(2, Matcher.sellList.get(0).getQuantity());
+    }
+
+    @Test
+    @DisplayName("Buy list empties after trade")
+    public void emptyBuyList() {
+        Matcher.createOrder("user1", 12, 6, "sell");
+        Matcher.createOrder("user2", 12, 6, "buy");
+        assertEquals(0, Matcher.buyList.size());
+    }
+
+    @Test
+    @DisplayName("Sell list empties after trade")
+    public void emptySellList() {
+        Matcher.createOrder("user1", 12, 6, "sell");
+        Matcher.createOrder("user2", 12, 6, "buy");
+        assertEquals(0, Matcher.sellList.size());
+    }
+
+    @Test
+    @DisplayName("Removes null order")
+    public void removeNullOrder() {
+        ArrayList<Order> newList = new ArrayList<>();
+        Order order1=new Order("user1", 2, 0, "buy");
+        Order order2=new Order("user2", 4, 2, "buy");
+        newList.add(order1);
+        newList.add(order2);
+        Matcher.removeNullOrders(newList);
+        assertEquals(1, newList.size());
+    }
+
+    @Test
+    @DisplayName("addOrder works for buy list")
+    public void addBuyOrder() {
+        Order order1=new Order("user1", 2, 8, "buy");
+        Order order2=new Order("user2", 4, 2, "buy");
+        Matcher.addOrder(order1);
+        Matcher.addOrder(order2);
+        assertEquals(2, Matcher.buyList.size());
+    }
+
+    @Test
+    @DisplayName("addOrder works for sell list")
+    public void addSellOrder() {
+        Order order1=new Order("user1", 2, 8, "sell");
+        Order order2=new Order("user2", 4, 2, "sell");
+        Matcher.addOrder(order1);
+        Matcher.addOrder(order2);
+        assertEquals(2, Matcher.sellList.size());
+    }
+
+    @Test
+    @DisplayName("newMatch adds to transHist")
+    public void matchOrders() {
+        Order order1=new Order("user1", 2, 8, "buy");
+        Order order2=new Order("user2", 2, 8, "sell");
+        Matcher.newMatch(order1, order2);
+        assertEquals(1, Matcher.transHist.size());
+    }
+
+    @Test
+    @DisplayName("newTrade adds to transHist")
+    public void tradeToTransHist() {
+        Matcher.newTrade("user1", "user2", 2, 8, "buy");
+        assertEquals(1, Matcher.transHist.size());
+    }
+
+    @Test
+    @DisplayName("processOrder adds to buyList")
+    public void processBuyOrder() {
+        Order order1=new Order("user1", 2, 8, "buy");
+        Matcher.processOrder(order1);
+        assertEquals(1, Matcher.buyList.size());
+    }
+
+    @Test
+    @DisplayName("processOrder adds to sellList")
+    public void processSellOrder() {
+        Order order1=new Order("user1", 2, 8, "sell");
+        Matcher.processOrder(order1);
+        assertEquals(1, Matcher.sellList.size());
+    }
+
+    @Test
+    @DisplayName("Private buy order book")
+    public void privBuyOrder() {
+        Matcher.createOrder("user1", 2, 8, "buy");
+        Matcher.privateOrderBuy("user1");
+        assertEquals(1, Matcher.privBuy.size());
+    }
+
+    @Test
+    @DisplayName("Private sell order book")
+    public void privSellOrder() {
+        Matcher.createOrder("user1", 2, 8, "sell");
+        Matcher.privateOrderSell("user1");
+        assertEquals(1, Matcher.privSell.size());
+    }
+
+    @Test
+    @DisplayName("Private trade history")
+    public void privTrans() {
+        Matcher.newTrade("user1", "user2", 2, 8, "buy");
+        Matcher.privateOrderTrans("user1");
+        assertEquals(1, Matcher.privTransHist.size());
+    }
+
+    @Test
+    @DisplayName("Agg buy book")
+    public void aggBuy() {
+        Matcher.createOrder("user1", 16, 4, "buy");
+        Matcher.createOrder("user2", 16, 6, "buy");
+        Matcher.aggregatedBuy();
+        Object [] keys = Matcher.aggBuy.keySet().toArray();  //price
+        Object [] values = Matcher.aggBuy.values().toArray();  //cumulative quantity
+        assertEquals((float)16.0,keys[0]);  //price
+        assertEquals(10,values[0]);  //cumulative quantity
+    }
+
+    @Test
+    @DisplayName("Agg sell book")
+    public void aggSell() {
+        Matcher.createOrder("user1", 14, 4, "sell");
+        Matcher.createOrder("user2", 14, 3, "sell");
+        Matcher.aggregatedSell();
+        Object [] keys = Matcher.aggSell.keySet().toArray();  //price
+        Object [] values = Matcher.aggSell.values().toArray();  //cumulative quantity
+        assertEquals((float)14.0,keys[0]);  //price
+        assertEquals(7,values[0]);  //cumulative quantity
+    }
 
 }
